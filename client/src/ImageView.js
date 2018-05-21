58cbc1ffaf93a70ccfc4fbd2674c02ad0c203228
Toby Jones <psytj1@exmail.nottingham.ac.uk>
2018-05-21 14:49:17 +0100

    Did some stuff on the server side

---
+++ client/src/ImageView.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import DATA from './data';
+
+class ImageView extends Component {
+	constructor() {
+		super();
+		this.state = { data: [] };
+	}
+
+	render() {
+		return (
+			<h1>It works!</h1>
+		);
+	}
+}
+
+export default ImageView;
